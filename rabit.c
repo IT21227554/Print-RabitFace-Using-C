dd0ae2abe583d286d5a875d447d1cfcaf321559d
Ray-Palmer <89266065+Ray-Palmer@users.noreply.github.com>
2021-09-18 15:28:19 +0530

    Final

---
+++ rabit.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main (void){
+	
+	printf("   ***       \n");
+	printf("  ** **\n");
+	printf(" **   **\n");	
+	printf(" **   **         **** \n");
+	printf(" **   **       **   ****\n");
+	printf(" **  **       *   **   **\n");
+	printf("  **  *      *  **  ***  **\n");
+	printf("   **  *    *  **     **  *\n");
+	printf("    ** **  ** **        **\n");
+	printf("    **   **  ** \n");
+	printf("   *           * \n");
+	printf("  *             * \n");
+	printf(" *    0     0    * \n");
+	printf(" *       0       * \n");
+	printf(" *    (     )    *\n");
+	printf("   *     W     *\n");
+	printf("     **     ** \n");
+	printf("       *****\n");
+	
+	
+	return 0;
+}
